13beb0150501977b438799e48eb781cc0c0722b9
JonathanChen <jonny29009@yahoo.com>
2020-01-14 13:01:30 -0800

    add GET request for all posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
+const db = require('../data/db');
 
 router.get('/', (req, res) => {
-    res.status(200).json('Hello');
+    db
+        .find()
+        .then(posts => {
+            res
+                .status(200)
+                .json({ posts: posts });
+        })
+        .catch(() => {
+            res
+                .status(500)
+                .json({ errorMessage: 'The posts information could not be retrieved' });
+        })
 })
 
 module.exports = router;
