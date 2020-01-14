b84a32ab5fecea881913a3199649cf923c715f49
JonathanChen <jonny29009@yahoo.com>
2020-01-14 13:09:01 -0800

    add GET request for a specific post

--- routes/posts.js
+++ routes/posts.js
@@ -16,4 +16,29 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    db
+        .findById(id)
+        .then(post => {
+            if(post) {
+                res
+                    .status(200)
+                    .json(post)
+            }
+            else {
+                res
+                    .status(404)
+                    .json({ errorMessage: 'The post with the specified ID does not exist.' });
+            }
+        })
+        .catch(() => {
+            res
+                .status(500)
+                .json({ errorMessage: 'The post information could not be retrieved.' })
+        })
+})
+
+
+
 module.exports = router;
