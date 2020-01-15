a17f0dc4e292a5bd7b28f20105e5e1e069fe1739
JonathanChen <jonny29009@yahoo.com>
2020-01-14 16:07:35 -0800

    add POST request for adding a new post

--- routes/posts.js
+++ routes/posts.js
@@ -78,4 +78,38 @@ router.get('/:id/comments', (req, res) => {
         });
 })
 
+router.post('/', (req, res) => {
+    const post = req.body;
+
+    if(!post.title || !post.contents) {
+        res
+            .status(400)
+            .json({ errorMessage: "Please provide title and contents for the post." });
+    }
+
+    db
+        .insert(post)
+        .then(({ id }) => {
+            db
+                .findById(id)
+                .then(newPost => {
+                    res
+                        .status(201)
+                        .json(newPost)
+                })
+                .catch(() => {
+                    res
+                        .status(404)
+                        .json({ errorMessage: "The post with the specified ID does not exist." });
+                })
+        })
+        .catch(() => {
+            res
+                .status(500)
+                .json({ errorMessage: "There was an error while saving the post to the database" });
+        });
+})
+
+
+
 module.exports = router;
